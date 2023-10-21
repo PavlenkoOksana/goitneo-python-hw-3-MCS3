969de6293bc048c428aa4595afad17fa25c8e033
PavlenkoOksana <pavlenko.oksana.od@gmail.com>
2023-10-21 14:46:33 +0200

    21.10.2023

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import pickle
 from collections import UserDict
 from collections import defaultdict
 from datetime import datetime
@@ -26,6 +27,7 @@ class Phone(Field):
             print("\033[31m{}\033[0m".format("The number must consist of 10 digits. try again"))
         else:
             return phone
+
 
 class Birthday(Field):
     def __init__(self, value):
@@ -57,17 +59,13 @@ class Record:
         if self.birthday == None and Birthday(birthday).valid_birthday(birthday):
            birthday_to_data = datetime.strptime(birthday, '%d.%m.%Y')
            self.birthday = birthday_to_data.strftime("%d.%m.%Y")
+           return "\033[32m{}\033[0m".format(f"{self.name}'s birthday added.")
         elif self.birthday != None: 
-            print("\033[31m{}\033[0m".format(f"The contact's {self.name} <birthday> field is already filled in")) 
-
+            return "\033[31m{}\033[0m".format(f"The contact's {self.name} <birthday> field is already filled in") 
+        
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, birthday: {self.birthday}"
-        # if self.birthday == None:
-        #     return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-        # else:
-        #     birthday_in_ddmmyy = self.birthday.strftime("%d.%m.%Y")
-        #     return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, birthday: {birthday_in_ddmmyy}"
-        
+               
     def remove_phone(self, phone):
         for p in self.phones:
             if p.value == phone:
@@ -83,12 +81,12 @@ class Record:
             for p in self.phones:
                 if p.value == old_phone:
                     p.value = new_phone
+                    return "\033[32m{}\033[0m".format("Contact changed.")
+                else:
+                    return "\033[31m{}\033[0m".format("Please enter a correct phone.")
 
 
 class AddressBook(UserDict):    
-    # def __init__(self):
-    #     self.data = UserDict()
-    
     def add_record (self, record):
         self.data[record.name.value] = record
 
@@ -97,25 +95,9 @@ class AddressBook(UserDict):
         for cont in self.data:
             if str(cont).lower() == name:
                 return self.data.get(cont)
-    
-    # def change(self, name, new_phone):
-    #     if Phone(new_phone).valid_phone(new_phone):
-    #         for cont in self.data:
-    #             if str(cont) == name:
-    #                 print("...self.data.get(cont).....", self.data.get(cont).phones, type(self.data.get(cont)))
-    #                 if self.data.get(cont).phones == []:
-    #                     record = self.data.get(cont)
-    #                     record.phones.append(new_phone)
-    #                     print("...self.data.get(cont).....", self.data.get(cont).phones, type(self.data.get(cont)))
-    #                     print(record)
-    #                     #return record.phones[0].value
-    #                 else:
-    #                     record = self.data.get(cont)
-    #                     record.phones[0].value = new_phone
-    #                     print("record.phones[0].value",record.phones[0].value)
-    #                     #return record.phones[0].value
-
-              
+        else:
+            raise KeyError
+        
     def delete(self, name):
         cont_for_del=""
         for cont in self.data:
@@ -132,7 +114,6 @@ class AddressBook(UserDict):
                 continue
             name = record.name.value
             birthday = datetime.strptime(record.birthday, '%d.%m.%Y').date()
-            #birthday = record.birthday
             birthday_this_year: datetime = birthday.replace(year=today.year)
             if birthday_this_year < today:
                 birthday_this_year = birthday_this_year.replace(year = today.year + 1)
@@ -143,16 +124,27 @@ class AddressBook(UserDict):
                 else:
                     birthdays_per_week[birthday_this_year.strftime("%A")].append(name)      
         if len(birthdays_per_week) == 0:
-            print("no birthdays next week")
+            return "No birthdays next week."
         else:
             print("birthdays_per_week",birthdays_per_week)
-            for weekday, user in birthdays_per_week.items():
+            for weekday, user in birthdays_per_week.items():                
                 print("\033[36m{}".format(weekday)+"\033[30m{}".format(": " + ", ".join(user)))
+            return "\033[1m\033[36m{}\033[0m".format("Don't forget to wish your colleagues a happy birthdays!")
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    def close(self):
+        self.fh.close()
 
+    def save_to_file(self):
+        with open('address_book_txt.txt', "wb") as fh:
+            pickle.dump(self, fh)
+        return "\033[32m{}\033[0m".format("Phone book saved to file: 'address_book_txt.txt'.")  
+                    
+    def read_from_file(self):
+      with open('address_book_txt.txt', "rb") as fh:
+        unpacked = pickle.load(fh)
+      print ("\033[32m{}\033[0m".format("The phone book was loaded from a file: 'address_book_txt.txt'.")) 
+      return unpacked
+ 
 
 def parse_input(user_input):
     if not user_input:
@@ -174,7 +166,6 @@ def input_error(func):
             return "\033[31m{}\033[0m".format("Please enter a correct name.")
         except:
             return "\033[31m{}\033[0m".format("Oops, something went wrong, try again.")
-
     return inner
 
 
@@ -189,44 +180,58 @@ def add_contact(args, book):
 
 @input_error
 def change_username_phone(args, book):
-    name, phone = args
+    name, old_phone, new_phone = args
     name = name.lower()
-    if Phone(phone).valid_phone(phone):
+    if Phone(new_phone).valid_phone(new_phone):
         if book.find(name) != None:
             rez = book.find(name)
-            rez.phones[0].value = phone
-            return "\033[32m{}\033[0m".format("Contact changed.")
+            return rez.edit_phone(old_phone, new_phone)
         else:
             raise KeyError    
-    
-
+       
 
 @input_error
-def phone_username(args, contacts):
+def phone_username(args, book):
     name = args
     name[0] = name[0].lower()
-    if name[0] in contacts:
-        return contacts[name[0]]
-    else:
-        raise KeyError
+    res = book.find(name[0])
+    return res.phones[0].value
     
 
 @input_error
 def all_phone_print(book):
     for name, record in book.data.items():
         print(record)
-    #return str[:-1]  
+
+
+@input_error
+def add_birthday(args, book):
+    name, birthday = args
+    name = name.lower()
+    res = book.find(name)
+    return res.add_birthday(birthday)
+     
+
+@input_error
+def show_birthday(args, book):
+    name = args
+    name[0] = name[0].lower()
+    res = book.find(name[0])
+    return res.birthday
+
+
+@input_error
+def birthdays(book):
+    return book.get_birthdays_per_week()
 
 
 @input_error
 def main():
     book = AddressBook()
-    contacts = {}
     print("\033[1m\033[34m{}\033[0m".format("Welcome to the assistant bot!"))
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-
         if command in ["close", "exit"]:
             print("\033[34m{}\033[0m".format("Good bye!"))
             break
@@ -236,12 +241,22 @@ def main():
             print(add_contact(args, book))
         elif command == "change":
             print(change_username_phone(args, book))
-        # elif command == "phone":
-        #     print(phone_username(args, contacts)) 
+        elif command == "phone":
+            print(phone_username(args, book)) 
         elif command == "all":
-            all_phone_print(book)             
-        # elif user_input == "" or command == "":
-        #     print("\033[31m{}\033[0m".format("You did not enter the command, please try again."))
+            all_phone_print(book)  
+        elif command == "add-birthday":
+            print(add_birthday(args, book))   
+        elif command == "show-birthday":
+            print(show_birthday(args, book))   
+        elif command == "birthdays":
+            print(birthdays(book))
+        elif command == "dump":
+            print (book.save_to_file())  
+        elif command == "load":
+            book = book.read_from_file()                      
+        elif user_input == "" or command == "":
+            print("\033[31m{}\033[0m".format("You did not enter the command, please try again."))
         else:
             print("\033[31m{}\033[0m".format("Invalid command, please try again."))
 
